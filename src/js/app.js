0b6ece7427927845fe268eb80b6453062fda9b02
b3m3 <b3m3@yandex.ru>
2022-06-12 19:24:48 +0300

    func createCurrencyNavItem

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
+  const currencyNavWrapp = document.querySelector('.currency-nav__list');
+
   const url = 'https://openexchangerates.org/api/latest.json?app_id=59c3f4f34d484f97b4cfd87dbd0825d4';
 
   const getData = async url => {
     const res = await fetch(url);
     const data = await res.json();
     return await data.rates;
+  };
+
+  const createCurrencyNavItem = (wrapp, name, rate) => {
+    const li = document.createElement('li');
+    li.classList.add('currency-nav__li');
+    li.innerHTML = `
+      <span class="currency-nav__name">${name}</span>
+      <span class="currency-nav__rate">${rate}</span>
+    `;
+    wrapp.append(li);
   };
 
   getData(url)

a865dbfe836a4065c09b074b7b215fa0053fe0b7
b3m3 <b3m3@yandex.ru>
2022-06-12 19:21:48 +0300

    func getData

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,19 @@
-import webp from './lib/webp.js';
-
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
-  webp();
+  const url = 'https://openexchangerates.org/api/latest.json?app_id=59c3f4f34d484f97b4cfd87dbd0825d4';
+
+  const getData = async url => {
+    const res = await fetch(url);
+    const data = await res.json();
+    return await data.rates;
+  };
+
+  getData(url)
+    .then(rates => {
+ 
+    })
+    .catch(err => {
+
+    });
 });

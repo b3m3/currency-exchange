1ff1bc94300e39cd68d186a399a10457e756fbd3
b3m3 <b3m3@yandex.ru>
2022-06-13 11:10:58 +0300

    scroll currency items

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
   const currencyNavWrapp = document.querySelector('.currency-nav__list');
+  const container = document.querySelector('.app__wrapp');
 
   const url = 'https://openexchangerates.org/api/latest.json?app_id=59c3f4f34d484f97b4cfd87dbd0825d4';
 
@@ -23,9 +24,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
   getData(url)
     .then(rates => {
- 
-    })
-    .catch(err => {
+      for (const key in rates) {
+        createCurrencyNavItem(currencyNavWrapp, key, (rates[key]).toFixed(3));
+      }
 
+      let px = 0;
+      const end = currencyNavWrapp.clientWidth - container.clientWidth;
+
+      setInterval(() => {
+        if (px < end) {
+          px++;
+        } else {
+          px = 0;
+        }
+
+        currencyNavWrapp.style.transform = `translate(-${px}px)`;
+      }, 16);
+    })
+    .catch(() => {
+      currencyNavWrapp.innerHTML = 'Opps... pls check back later';
     });
 });
